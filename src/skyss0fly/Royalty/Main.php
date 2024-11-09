99f1e76e49c0ab8258e5b401238f35b8577a5486
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-11-09 14:34:35 +1000

    Create Main.php

---
+++ src/skyss0fly/Royalty/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace skyss0fly\Royalty;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\VanillaItems;
+
+class Main extends PluginBase {
+
+
+}
+
+?>
